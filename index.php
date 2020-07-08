64cc4cab92ca4b226aa590d8d8c78cae892afc60
Noor M. I. Albardawil <67733645+noor2020-eng@users.noreply.github.com>
2020-07-08 16:34:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'person.php'?>
+<!doctype html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+
+</head>
+
+<body>
+
+	<?php
+
+		
+
+	$pet01 = new Person();
+	echo $pet01->owner();
+	
+	?>
+</body>
+
+</html>
